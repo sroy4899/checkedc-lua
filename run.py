59358b1797df7088b06ad45a5e56f82e10236bc2
Shilpa Roy <shilparoy4899@gmail.com>
2020-07-03 11:51:17 -0400

    converted with BigRefactor b4b3171f1acd130ddaa6231e70caa3dd4c0ffaf8

--- run.py
+++ run.py
@@ -15,38 +15,14 @@ def update_script_locations(cconv_loc, include_loc, python_loc):
         elif "INCLUDES=" in line: 
             line = "INCUDES=" + include_loc
         sys.stdout.write(line) 
-    for line in fileinput.input(filename, inplace=1):
+    for line in fileinput.input("replace.py", inplace=1):
         if "#!" in line: 
             line = "#!" + python_loc 
         sys.stdout.write(line)
-
-
-def run_diff_command(command, filename): 
-    os.system(command)  
-    addon = "" 
-    if filename: 
-        addon = "for the file {}".format(filename)
-    file = open("GIT_DIFF_GENERATOR.txt", "r") 
-    empty = True 
-    for line in file.readlines(): 
-        if(empty): 
-            empty = False 
-            print("\nThe following diffs were generated " + addon + "\n\n") 
-        print(line) 
-    if empty: 
-        print("No diffs to report!")
-    os.system("rm GIT_DIFF_GENERATOR.txt")
-
-
-def run_file_by_file(): 
-    f = []
-    for file in os.listdir("./"): 
-        if file.endswith(".c"):
-            f.extend(file)
-    for filename in f: 
-        command = "git diff baseline-converted..baseline --output=GIT_DIFF_GENERATOR.txt -- {}".format(filename) 
-        run_diff_command(command, filename)
-
+    for line in fileinput.input("update_database.py", inplace=1):
+        if "#!" in line: 
+            line = "#!" + python_loc 
+        sys.stdout.write(line)
 
 # asking the user information about their path locations
 print("Please provide the path to the cconv-standalone tool:") 
@@ -69,8 +45,11 @@ print("##########\trunning the conversion tool...\t##########")
 os.system("sh convert_all.sh")
 
 print("##########\treplacing the output\t##########")
-os.system("python replace.py")
+os.system("python3 replace.py")
 
+
+
+exit()
 compiles = True
 print("##########\tcompiling...\t##########")
 out = subprocess.Popen(['make', 'CC={}'.format(clang_loc), '-i'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT) 
@@ -81,42 +60,3 @@ if "error:" in stdout:
 if not compiles: 
     print("\nFailure to compile!\n")
 os.system("rm *.o")  
-
-
-# reset everything 
-os.system("git reset --HARD") 
-update_script_locations(cconv_loc, include_loc, python_loc) 
-for line in fileinput.input("convert_all.sh", inplace=1):
-    if "output-postfix" in line: 
-        line = "-output-postfix=checked \\\n-alltypes \\"
-    sys.stdout.write(line) 
-print("##########\tupdating the database with your locations...\t##########")
-os.system("python3 update_database.py") 
-
-print("##########\trunning the conversion tool...\t##########")
-os.system("sh convert_all.sh")
-
-print("##########\treplacing the output\t##########")
-os.system("python replace.py")
-
-
-
-### Interactive version of the diff checker for regressions : support coming soon
-menu = """
-####################################################################################
-Press
-[1]\tfor a raw diff between the latest conversion and your conversion
-[2]\tfor a diff that goes through the files individually (skips files without diffs)
-[<filename>]\tif you'd like to view a diff for a specific file
-[q]\tto quit
-####################################################################################
-"""
-print(menu)
-choice = input("[CHOICE] > ").rstrip()
-if choice=="1": 
-    command = "git diff baseline-converted..baseline --output=GIT_DIFF_GENERATOR.txt -- '*.c'" 
-    run_diff_command(command, None)
-elif choice=="2": 
-    run_file_by_file()
-elif choice=="q": 
-    print("All done!")
